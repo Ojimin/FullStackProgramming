b234608f67c1d709905d73cd4e430fe5490e8cc8
Ojimin <ogm0205@gmail.com>
2024-05-26 03:59:13 +0900

    feat: install go

---
+++ helloworld.go
@@ -0,0 +1,8 @@
+package main //패키지는 여러 코드를 하나로 묶는 역할
+//go로 작성된 프로그램은 main package를 포함해야하며 프로그램 실행시 main package의 main함수가 실행되게 됨
+
+import "fmt" //문자열 출력 역할
+
+func main() {
+	fmt.Println("hello world")
+}
