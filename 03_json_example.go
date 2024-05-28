5caffca222379aedba2df60bc9d9423b9df1aed7
Ojimin <ogm0205@gmail.com>
2024-05-28 19:20:33 +0900

    feat: go to json file

--- 03_json_example.go
+++ 03_json_example.go
@@ -7,21 +7,21 @@ import (
 	"os"
 )
 
-type SuperHero struct {
-	SquadName string `json:"squadName"`
-	HomeTown string `json:"homeTown"`
-	Formed int `json:"formed"`
-	SecretBase string `json:"secretBase"`
-	Active bool `json:"active"`
-	Members [] struct {
-		Name string `json:"name"`
-		Age int `json:"age"`
-		SecretIdentity string `json:"secretIdentity"`
-		Powers []string `json:"powers"`
-	} `json:"members"`
-}
+// type SuperHero struct {
+// 	SquadName string `json:"squadName"`
+// 	HomeTown string `json:"homeTown"`
+// 	Formed int `json:"formed"`
+// 	SecretIdentity string `json:"secretBase"`
+// 	Active bool `json:"active"`
+// 	Members [] struct {
+// 		Name string `json:"name"`
+// 		Age int `json:"age"`
+// 		SecretIdentity string `json:"secretIdentity"`
+// 		Powers []string `json:"powers"`
+// 	} `json:"members"`
+// }
 
-func main() {
+func json_to_go() {
 	file, err := os.Open("03_json_example.json")
 	if err != nil {
 		return 
--- 04_json_example.go
+++ 04_json_example.go
@@ -1,10 +1,88 @@
 // go 언어 to json 파일로 저장
 package main
 
-import "os"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
-func go_to_json() {
-	os.WriteFile()
+type SuperHero struct {
+	SquadName string `json:"squadName"`
+	HomeTown string `json:"homeTown"`
+	Formed int `json:"formed"`
+	SecretIdentity string `json:"secretBase"`
+	Active bool `json:"active"`
+	Members [] struct {
+		Name string `json:"name"`
+		Age int `json:"age"`
+		SecretIdentity string `json:"secretIdentity"`
+		Powers []string `json:"powers"`
+	} `json:"members"`
+}
 
+func main() {
+	
+	superHeroes := SuperHero {
+		SquadName: "Super hero squad",
+		HomeTown: "Metro City",
+		Formed: 2016,
+		SecretIdentity: "Super tower",
+		Active: true,
+		Members: []struct {
+			Name string `json:"name"`
+			Age int `json:"age"`
+			SecretIdentity string `json:"secretIdentity"`
+			Powers []string `json:"powers"`
+		}{
+			{
+				Name : "Molecule Man",
+				Age : 29,
+				SecretIdentity :  "Dan Jukes",
+				Powers : []string{"Radiation resistance",
+					"Turning tiny", 
+					"Radiation blast",
+			},
+		},
+		{
+			Name: "Madame Uppercut",
+			Age: 39,
+			SecretIdentity: "Jane Wilson",
+			Powers: []string{
+				"Million tonne punch",
+				"Damage resistance",
+				"Superhuman reflexes",
+			},
+		},
+		{
+			Name: "Eternal Flame",
+			Age: 1000000,
+			SecretIdentity: "Unknown",
+			Powers: []string{
+				"Immortality",
+				"Heat Immunity",
+				"Inferno",
+				"Teleportation",
+				"Interdimensional travel",
+			},	
+		},
+	  },
+	}
 
+		fmt.Println(superHeroes.HomeTown)
+		fmt.Println(superHeroes.Active)
+		fmt.Println(superHeroes.Members[1].Powers[2])
+		
+		jsonData, err := json.MarshalIndent(superHeroes, "", "  ") //go to json 변환
+
+		file, err := os.Create("04_json_example.json")
+		if err != nil {
+			panic(err)
+		} 
+		defer file.Close()
+
+		_, err = file.Write(jsonData)
+		if err != nil {
+			return
+		}
 }
