d9a23e85af516ad9c223f4d22dc7ce0ef4cbb6ed
Ojimin <ogm0205@gmail.com>
2024-05-28 21:29:14 +0900

    feat: json string to go struct type

--- 05_json_example.go
+++ 05_json_example.go
@@ -6,67 +6,67 @@ import (
 	"fmt"
 )
 
-type SuperHero struct {
-	SquadName string `json:"squadName"`
-	HomeTown string `json:"homeTown"`
-	Formed int `json:"formed"`
-	SecretIdentity string `json:"secretBase"`
-	Active bool `json:"active"`
-	Members [] struct {
-		Name string `json:"name"`
-		Age int `json:"age"`
-		SecretIdentity string `json:"secretIdentity"`
-		Powers []string `json:"powers"`
-	} `json:"members"`
-}
+// type SuperHero struct {
+// 	SquadName string `json:"squadName"`
+// 	HomeTown string `json:"homeTown"`
+// 	Formed int `json:"formed"`
+// 	SecretIdentity string `json:"secretBase"`
+// 	Active bool `json:"active"`
+// 	Members [] struct {
+// 		Name string `json:"name"`
+// 		Age int `json:"age"`
+// 		SecretIdentity string `json:"secretIdentity"`
+// 		Powers []string `json:"powers"`
+// 	} `json:"members"`
+// }
 
-func main() {
+func json_to_go() {
 
-	superHeroes_source := SuperHero {
-		SquadName: "Super hero squad",
-		HomeTown: "Metro City",
-		Formed: 2016,
-		SecretIdentity: "Super tower",
-		Active: true,
-		Members: []struct {
-			Name string `json:"name"`
-			Age int `json:"age"`
-			SecretIdentity string `json:"secretIdentity"`
-			Powers []string `json:"powers"`
-		}{
-			{
-				Name : "Molecule Man",
-				Age : 29,
-				SecretIdentity :  "Dan Jukes",
-				Powers : []string{"Radiation resistance",
-					"Turning tiny", 
-					"Radiation blast",
-			},
-		},
-		{
-			Name: "Madame Uppercut",
-			Age: 39,
-			SecretIdentity: "Jane Wilson",
-			Powers: []string{
-				"Million tonne punch",
-				"Damage resistance",
-				"Superhuman reflexes",
-			},
-		},
-		{
-			Name: "Eternal Flame",
-			Age: 1000000,
-			SecretIdentity: "Unknown",
-			Powers: []string{
-				"Immortality",
-				"Heat Immunity",
-				"Inferno",
-				"Teleportation",
-				"Interdimensional travel",
-			},	
-		},
-	  },
-	}
+	// superHeroes_source := SuperHero {
+	// 	SquadName: "Super hero squad",
+	// 	HomeTown: "Metro City",
+	// 	Formed: 2016,
+	// 	SecretIdentity: "Super tower",
+	// 	Active: true,
+	// 	Members: []struct {
+	// 		Name string `json:"name"`
+	// 		Age int `json:"age"`
+	// 		SecretIdentity string `json:"secretIdentity"`
+	// 		Powers []string `json:"powers"`
+	// 	}{
+	// 		{
+	// 			Name : "Molecule Man",
+	// 			Age : 29,
+	// 			SecretIdentity :  "Dan Jukes",
+	// 			Powers : []string{"Radiation resistance",
+	// 				"Turning tiny", 
+	// 				"Radiation blast",
+	// 		},
+	// 	},
+	// 	{
+	// 		Name: "Madame Uppercut",
+	// 		Age: 39,
+	// 		SecretIdentity: "Jane Wilson",
+	// 		Powers: []string{
+	// 			"Million tonne punch",
+	// 			"Damage resistance",
+	// 			"Superhuman reflexes",
+	// 		},
+	// 	},
+	// 	{
+	// 		Name: "Eternal Flame",
+	// 		Age: 1000000,
+	// 		SecretIdentity: "Unknown",
+	// 		Powers: []string{
+	// 			"Immortality",
+	// 			"Heat Immunity",
+	// 			"Inferno",
+	// 			"Teleportation",
+	// 			"Interdimensional travel",
+	// 		},	
+	// 	},
+	//   },
+	// }
 
 	data, err := json.MarshalIndent(superHeroes_source, "", "  ") //go to json
 	if err!=nil {
--- 06_json_example.go
+++ 06_json_example.go
@@ -1,1 +1,71 @@
-//json 문자열을 go 데이터로 변환 및 출력
+// json 문자열을 go 데이터로 변환 및 출력
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+type SuperHero struct {
+	SquadName string `json:"squadName"`
+	HomeTown string `json:"homeTown"`
+	Formed int `json:"formed"`
+	SecretBase string `json:"secretBase"`
+	Active bool `json:"active"`
+	Members [] struct {
+		Name string `json:"name"`
+		Age int `json:"age"`
+		SecretIdentity string `json:"secretIdentity"`
+		Powers []string `json:"powers"`
+	} `json:"members"`
+}
+
+func main() {
+	superHeroes_source := `{
+		"squadName": "Super hero squad",
+		"homeTown": "Metro City",
+		"formed": 2016,
+		"secretBase": "Super tower",
+		"active": true,
+		"members": [
+		  {
+			"name": "Molecule Man",
+			"age": 29,
+			"secretIdentity": "Dan Jukes",
+			"powers": [
+				"Radiation resistance",
+				"Turning tiny",
+				"Radiation blast"
+			]
+		  },
+		  {
+			"name": "Madame Uppercut",
+			"age": 39,
+			"secretIdentity": "Jane Wilson",
+			"powers": [
+				"Million tonne punch",
+				"Damage resistance",
+				"Superhuman reflexes"
+			]
+		  },
+		  {
+			"name": "Eternal Flame",
+			"age": 1000000,
+			"secretIdentity": "Unknown",
+			"powers": [
+				"Immortality",
+				"Heat Immunity",
+				"Inferno",
+				"Teleportation",
+				"Interdimensional travel"
+			]
+		  }
+		]
+	}`
+	var superHeroes SuperHero
+	err := json.Unmarshal([]byte(superHeroes_source),&superHeroes) //json to 문자열
+	if err!=nil {
+		fmt.Println("JSON 디코딩 실패:", err)
+		return
+	}   
+	fmt.Println(superHeroes.HomeTown)
+}
